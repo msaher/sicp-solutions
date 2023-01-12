88236fd2a079e0b3300106012632aff66b156d6a
saher <msaher.shair@gmail.com>
2023-01-12 20:47:57 +0300

    added 1.30.scm

---
+++ 1.30.scm
@@ -0,0 +1,11 @@
+#!/bin/guile -l
+!#
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b) result
+	(iter (next a) (+ a result))))
+    (iter a 0))
+
+(define (sum-int a b)
+  (sum (lambda (x) x) a (lambda (x) (+ x 1)) b))
