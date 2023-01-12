aaa2f0286b448d4576bb2e85d62f8f804e7ba207
saher <msaher.shair@gmail.com>
2023-01-12 20:47:58 +0300

    added 3.34.scm

---
+++ 3.34.scm
@@ -0,0 +1,3 @@
+;; suppose you use (multiplier a a b). If `b` has a value, we can easily find
+;; the value of `a` in real life, but our current program can not, because it
+;; does not know about square roots
