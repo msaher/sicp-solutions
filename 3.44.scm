0850c48c371dd7599d3990c5e9aecdda560bba24
saher <msaher.shair@gmail.com>
2023-01-12 20:47:58 +0300

    added 3.44.scm

---
+++ 3.44.scm
@@ -0,0 +1,3 @@
+;; Ben is right. There's no problem with this implementation. The difference
+;; here being that `amount` we transfer is not dependent on the balance, In
+;; other words, it does not require that the state of balance to not change.
