85f3dd2c274a0ce11e6154a91477f879ed1de954
saher <msaher.shair@gmail.com>
2023-01-12 20:47:58 +0300

    added 3.14.scm

---
+++ 3.14.scm
@@ -0,0 +1,25 @@
+;; prediction: it reverses a list. Spoilers: I was right :D
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+      y
+      (let ((temp (cdr x)))
+        (set-cdr! x y)
+        (loop temp x))))
+  (loop x '()))
+
+; (loop '(a b c d) '())
+;         temp = '(b c d)
+;         x = (a)
+;         (loop (b c d) (a))
+;                 temp (c d)
+;                 x = (b a)
+;                 (loop (c d) (b a))
+;                         temp = (d)
+;                         x = (c b a)
+;                         (loop (d) (c b a))
+;                                 temp = ()
+;                                 x = (d c b a)
+;                                 (loop () (d c b a))
+;                                         (d c b a))
