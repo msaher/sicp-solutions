24e702812e5dd98d64471db0de349ddc8e5d26f2
saher <msaher.shair@gmail.com>
2023-01-12 20:47:57 +0300

    added 2.12.scm

---
+++ 2.12.scm
@@ -0,0 +1,25 @@
+#!/bin/guile -l
+!#
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound x)
+  (car x))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (upper-bound x)
+  (cdr x))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-center-percent c p)
+  (make-center-width c  (* p c)))
+(define (percent i)
+  (/ (width i) (center i)))
