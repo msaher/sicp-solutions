49103ab941d630e2fbcff8bd59ceeb0bd5e31c23
saher <msaher.shair@gmail.com>
2023-01-12 20:47:58 +0300

    added 3.41.scm

---
+++ 3.41.scm
@@ -0,0 +1,3 @@
+;; I don't agree with Ben. Unlike deposit and withdraw, when 'balance is
+;; supplied, balance returns a number, which is *primitive*, and therefore can
+;; not be interrupted, so there's no need to protect it
